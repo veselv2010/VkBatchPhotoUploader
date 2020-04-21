af96a00a90e523253d75a106ce65c7f11d0aadc9
Никита Веселов <veselv2010@gmail.com>
2020-04-21 11:54:54 +0300

    removed unused namespaces

--- VkBatchPhotoUploader/Program.cs
+++ VkBatchPhotoUploader/Program.cs
@@ -1,17 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Runtime.InteropServices;
-using System.Threading.Tasks;
-using System.Text;
-using System.Threading;
-using System.Windows.Forms;
-using VkNet;
-using VkNet.Model;
-using Newtonsoft.Json.Linq;
 
 namespace VkBatchPhotoUploader
 {
